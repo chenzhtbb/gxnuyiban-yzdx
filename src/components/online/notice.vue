afad5a96465e68d7a5d82d2bab9ff0b9a985e2ed
chenzhtbb <chenzhtbb@163.com>
2017-11-12 10:56:51 +0800

    更新了学工在线内容

--- src/components/online/notice.vue
+++ src/components/online/notice.vue
@@ -1,6 +1,9 @@
 <template>
   <toggle>
     <div class="notice">
+      <div class="link" v-if="item.link">
+        <iframe :src="item.link"></iframe>
+      </div>
       <scroll style="height: 100%;" :data="item">
         <div style="margin: 8px;">
           <div class="top">
@@ -14,8 +17,11 @@
               <span>{{item.time}}</span>
             </div>
           </div>
-          <div class="content">
+          <div class="content" v-if="!item.link">
             <div class="text" v-html="item.content"></div>
+          </div>
+          <div v-else>
+            <loading :title="title"></loading>
           </div>
         </div>
       </scroll>
@@ -26,11 +32,37 @@
 <script type="text/ecmascript-6">
   import Toggle from 'base/toggle/toggle'
   import Scroll from 'base/scroll/scroll'
+  import Loading from 'base/loading/loading'
+  import { getXgbNotice } from 'api/xgb'
 
   export default {
     components: {
       Toggle,
-      Scroll
+      Scroll,
+      Loading
+    },
+    data () {
+      return {
+        item: [],
+        title: ''
+      }
+    },
+    activated () {
+      setTimeout(() => {
+        this.key = this.$route.query.page
+        this.getXgbNotice()
+      }, 20)
+    },
+    deactivated () {
+      this.item = []
+    },
+    methods: {
+      getXgbNotice () {
+        getXgbNotice(this.key).then((res) => {
+          this.item = res
+          this.title = `小易正在带路前往${res.title}页面`
+        })
+      }
     }
   }
 </script>
@@ -40,6 +72,26 @@
 
   img
     width 100% !important
+
+  iframe
+    border 0
+    position fixed
+    top 0
+    bottom 0
+    left 0
+    right 0
+    width 100%
+    height 100%
+
+  .link
+    position fixed
+    top 0
+    bottom 0
+    left 0
+    right 0
+    width 100%
+    height 100%
+    z-index 998
 
   .notice
     background #FFFFFF
