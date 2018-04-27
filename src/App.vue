62e5ccf55b51d634024e514cb6cd9759f591e0d8
chenzhtbb <chenzhtbb@163.com>
2018-04-27 09:44:00 +0800

    提交测试

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,3 @@
     </keep-alive>
   </div>
 </template>
-
-<script type="text/ecmascript-6">
-  import { browser } from 'common/js/ybh5'
-
-  export default {
-    mounted () {
-      setTimeout(() => {
-        if (process.env.NODE_ENV === 'production' && !browser.versions.mobile) {
-          window.location.href = 'http://yiban.gxnu.edu.cn/score'
-        }
-      }, 20)
-    }
-  }
-</script>
